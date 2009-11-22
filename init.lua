7258e3ded46f0ad4efb3666603fadfde22495315
Henk Boom <henk@henk.ca>
2009-11-22 15:21:31 -0500

    new faster collision detection

--- init.lua
+++ init.lua
@@ -10,8 +10,69 @@ local creatures = require 'creatures'
 import 'gl'
 import 'dokidoki.base'
 
+function nwipe(t)
+  for i = 1, t.n do
+    t[i] = nil
+  end
+end
+
+function init_collision_detection(game, tags)
+  local cell_size = 32
+  local grids
+
+  game.add_actor{
+    preupdate = function ()
+      grids = {}
+      for _, tag in ipairs(tags) do
+        grids[tag] = grids[tag] or {}
+        local grid = grids[tag]
+        for _, a in ipairs(game.get_actors_by_tag(tag)) do
+          local i = math.floor(a.pos.x / cell_size)
+          local j = math.floor(a.pos.y / cell_size)
+          grid[i] = grid[i] or {}
+          local col = grid[i]
+          col[j] = col[j] or {n=0}
+          local bucket = col[j]
+          bucket[bucket.n + 1] = a
+          bucket.n = bucket.n + 1
+        end
+      end
+    end
+  }
+
+  function game.nearby(pos, radius, tag)
+    local mini = math.floor((pos.x - radius) / cell_size)
+    local maxi = math.ceil((pos.x + radius) / cell_size)
+    local minj = math.floor((pos.y - radius) / cell_size)
+    local maxj = math.ceil((pos.y + radius) / cell_size)
+
+    local grid = grids[tag]
+    assert(grid, 'game.nearby called on non-indexed tag')
+    local actors = {}
+    local n = 1
+
+    for i = mini, maxi do
+      if grid[i] then
+        for j = minj, maxj do
+          --game.trace_circle(pos, v2((i+0.5) * cell_size, (j+0.5) * cell_size), 2)
+          if grid[i][j] then
+            for _, a in ipairs(grid[i][j]) do
+              if v2.sqrmag(a.pos - pos) <= radius * radius then
+                actors[n] = a
+                n = n + 1
+              end
+            end
+          end
+        end
+      end
+    end
+
+    return actors
+  end
+end
+
 kernel.start_main_loop(actor_scene.make_actor_scene(
-  {'trace_cleanup', 'update'},
+  {'trace_cleanup', 'preupdate', 'update'},
   {'draw_setup', 'draw_outline', 'draw_fill', 'draw_inner_outline',
    'draw_inner_fill', 'draw_foreground', 'draw_trace'},
   function (game)
@@ -48,13 +109,7 @@ kernel.start_main_loop(actor_scene.make_actor_scene(
       game.add_actor(self)
     end
 
-    -- for collision detection
-    function game.nearby(pos, radius, tag)
-      local function close_enough(actor)
-        return v2.sqrmag(actor.pos - pos) <= radius * radius
-      end
-      return ifilter(close_enough, game.get_actors_by_tag(tag))
-    end
+    init_collision_detection(game, {'prey', 'foliage'})
 
     game.add_actor{
       draw_setup = function ()
@@ -90,11 +145,11 @@ kernel.start_main_loop(actor_scene.make_actor_scene(
         game,
         v2(math.random() * C.width, math.random() * C.height)))  
     end
-     for i = 1, 10 do 
-        game.add_actor(creatures.make_herbivore(
-          game,
-          v2(math.random() * C.width, math.random() * C.height)))  
-      end
+    for i = 1, 10 do 
+      game.add_actor(creatures.make_herbivore(
+        game,
+        v2(math.random() * C.width, math.random() * C.height)))  
+    end
     
   end))
 
